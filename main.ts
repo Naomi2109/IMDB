57dd24e58265f1bd107ca7987ab6e6ce02a5f8bf
Naomi2109 <naomi.gamba21@gmail.com>
2021-10-15 11:46:22 +0200

    primera parte

---
+++ main.ts
@@ -0,0 +1,8 @@
+import { Professional } from "./BBDD";
+
+let professional1 = new Professional ("Carlos", 34, "Masculino", 70, 170, "castaño", "azules", "arte dramatico", false, "español", 5, "actor")
+let professional2 = new Professional ("Estefanía", 25, "Femenino", 55, 160, "Pelirroja", "Marrones", "diseño grafico", false, "italiana", 0, "actriz")
+
+
+professional1.allAttributes()
+professional2.allAttributes()
