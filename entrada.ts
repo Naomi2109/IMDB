7d11f3338fd7c7f427c9d769c86f7bdf837f3aeb
Kikelon <correo_enrique@hotmail.com>
2021-10-18 11:48:08 +0200

    Externalización de funciones en clases y entrada por consoloa

--- entrada.ts
+++ entrada.ts
@@ -1,39 +1,10 @@
-import { Professional } from "./BBDD";
 import { Movie } from "./movie";
 import { Imdb } from "./imdb";
-
-let professional1 = new Professional ("Carlos", 34, "Masculino", 70, 170, "castaño", "azules", "arte dramatico", false, "español", 5, "actor")
-let professional2 = new Professional ("Estefanía", 25, "Femenino", 55, 160, "Pelirroja", "Marrones", "diseño grafico", false, "italiana", 0, "actriz")
-
-
-// professional1.allAttributes()
-// professional2.allAttributes()
-
 let peli1 = new Movie("El silencio de la ciudad blanca", 2019, "Española", "Drama");
 let peli2 = new Movie("Padre no hay más que uno", 2019, "Española", "Comedia");
-
-// peli1.allAttributes();
-// peli2.allAttributes();
-
 let archivo = new Imdb([peli1, peli2]);
-// console.log(archivo);
 
 
-
-let objeto = JSON.stringify(archivo)
-let fs =require("fs")
-fs.writeFileSync("./imdbBBDD.json", objeto, error => {
-    if (error)
-    console.log(error);
-    else
-    console.log("Archivo creado");
-})
-
-archivo.escribirEnFicheroJSON("imdb.json")
-console.log(archivo.peliculas);
-archivo.peliculas = [];
-archivo = archivo.obtenerInstanciaIMDB("imdb2.json");
-console.log(archivo.peliculas);
 
 const texto = ["Escribe el título de la película: ", "Escribe el año de estreno: ", "Escribe la nacionalidad de la película: ", "Escribe el género de la película: "];
 let respuestas = [];
@@ -58,9 +29,8 @@ process.stdin.on('data', data => {
 
 process.on('exit', function(){
     const peli = new Movie(respuestas[0], respuestas[1], respuestas[2], respuestas[3]);
-    console.log(peli);
-    console.log(archivo.peliculas);
-    // archivo.peliculas.push(peli);
+    archivo.peliculas.push(peli);
 });
 
 obtenerDatos(0);
+
--- imdb.ts
+++ imdb.ts
@@ -7,13 +7,22 @@ export class Imdb{
         this.peliculas = peliculas;
     }
     
-    // public escribirEnFicheroJSON(creaFichero:string)
-    // {
-    //     creaFichero = JSON.stringify()
-    // } 
+    public escribirEnFicheroJSON(nombreFichero:string)
+    {
+        let objeto = JSON.stringify(this);
+        let fs = require("fs");
+        fs.writeFileSync(nombreFichero, objeto, error => {
+            if (error)
+            console.log(error);
+            else
+            console.log("Archivo creado");
+        });
+       
+    } 
 
-    // public obtenerInstanciaIMDB(Fichero:string):Imdb
-    // {
-        
-    // }
+    public obtenerInstanciaIMDB(nombreFichero:string):Imdb{
+        let fs = require("fs");
+        let resultado = new Imdb(JSON.parse(fs.readFileSync(nombreFichero,"utf8")));
+        return resultado;        
+    }
 };
